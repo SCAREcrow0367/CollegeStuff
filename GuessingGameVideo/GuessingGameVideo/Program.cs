df80f19f4e224fd76495d231ef631888e51c4c56
SCAREcrow0367 <128756851+SCAREcrow0367@users.noreply.github.com>
2023-05-04 21:52:30 -0700

    Add files via upload
    Add to collection

---
+++ GuessingGameVideo/GuessingGameVideo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGameVideo
+{
+    class Program
+    {
+        static int guess;
+        static int answer;
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Guessing game C# Console";
+
+            introduction();
+            randomize();
+            while (true)
+            {
+                askData();
+                display();
+                if (guess == answer)
+                    break;
+            }
+            goodBye();
+        }
+        static void goodBye()
+        {
+            Console.WriteLine("thank you for using this Application");
+            Console.WriteLine("Press any key to end.");
+            Console.ReadLine();
+
+        }
+        static void introduction()
+        {
+            Console.WriteLine("Welcome to the guessing game C# console\nEnter any key to continue!");
+            Console.ReadLine();
+
+        }
+        static void askData()
+        {
+            Console.Clear();
+            counter++;
+            while (true)
+            {
+                Console.WriteLine("Please enter integer between 0-20");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess < 0 || guess > 20)
+                    Console.WriteLine("Invalid expression, please try a within proper range.");
+                else
+                    break;
+            }
+        }
+        static void randomize()
+        {
+            Random random = new Random();
+            answer = random.Next(0, 20);
+        }
+        static void display()
+        {
+            if (guess == answer)
+            {
+                Console.WriteLine("your answer is correct! Exceelent job!\nAttempts:" + counter);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect! try again!\nAttempts:" + counter);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ guess a number video/guess a number video/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guess_a_number_video
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string [] Words = { "Chess, Hockey, Basketball, Football, Baseball, Gymnastics, Tennis, Track" };
+            int randomWords;
+            int guess;
+            int numberofguesses = 5;
+            bool gameOver = false;
+
+            Console.WriteLine("I'm thinking of a word. What word is it?");
+            Console.WriteLine(Words);
+
+            while (gameOver == false)
+            {
+                guess = Convert.ToChar(Console.ReadLine());
+                numberofguesses--;
+
+                if (guess != randomWords && numberofguesses == 0)
+                {
+                    Console.WriteLine("I mean, after five tries. We might as well just stop.");
+                    gameOver = true;
+                }
+                else if (guess == randomWords)
+                {
+                    Console.WriteLine("That's it! You've cracked my word choice! YOU WIN!!!!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
