a25ea1c6165b264ac3fa8257f24a7f0f933f32a0
SCAREcrow0367 <128756851+SCAREcrow0367@users.noreply.github.com>
2023-05-04 21:50:12 -0700

    Add files via upload
    Add to collection

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace ValidID 
+{
+    class ValidID
+    {
+        static void Main()
+        {
+            int idNum;
+            string input;
+            const int LOW = 1000;
+            const int HIGH = 9999;
+
+                Write("Enter ID number:");
+                input = ReadLine();
+                idNum = Convert.ToInt32(input);
+
+                while (idNum < LOW || idNum > HIGH)
+                {
+                    WriteLine("{0} is an invalid ID number", idNum);
+                    Write("ID numbers must be");
+                    WriteLine("bewteen {0} and {1} inclusive", LOW, HIGH);
+                    Write("Enter an ID Number:");
+                    input = ReadLine();
+                    idNum = Convert.ToInt32(input);
+                    
+
+                }
+
+
+            WriteLine("ID number is {0} is valid", idNum);
+            input = ReadLine();
+            idNum = Convert.ToInt32(input);
+        }
+    }
+}
