00ff4c0c819554289750ec0daafd2cdb2d580673
SCAREcrow0367 <128756851+SCAREcrow0367@users.noreply.github.com>
2023-05-04 21:49:36 -0700

    Add files via upload
    Add to collection

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double total_price = 0;
+            double[] Shoe_Size = new double[10] { 0, .50, 1.00, 1.50, 2.00, 2.50, 3.00, 3.50, 4.00, 4.50 };
+            double ball_price = 3.00;
+            double hours_price = 10.00;
+            double hours_total = 0;
+            int hours = 0;
+           
+        
+            if (Shoe_Size1)
+                total_price += Shoe_Size[0];
+            else if (Shoe_Size2)
+                total_price += Shoe_Size[0];
+            else if (Shoe_Size2)
+                total_price += Shoe_Size[0];
+            else if (Shoe_Size3)
+                total_price += Shoe_Size[1];
+            else if (Shoe_Size4)
+                total_price += Shoe_Size[2];
+            else if (Shoe_Size5)
+                total_price += Shoe_Size[3];
+            else if (Shoe_Size6)
+                total_price += Shoe_Size[4];
+            else if (Shoe_Size7)
+                total_price += Shoe_Size[5];
+            else if (Shoe_Size8)
+                total_price += Shoe_Size[6];
+            else if (Shoe_Size9)
+                total_price += Shoe_Size[7];
+            else if (Shoe_Size10)
+                total_price += Shoe_Size[8];
+            else if (Shoe_Size11)
+                total_price += Shoe_Size[9];
+            if (ballBtn)
+                total_price += ball_price;
+            
+
+
+            
+        }
+    }
+}
