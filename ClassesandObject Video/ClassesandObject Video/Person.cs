acb3daa8604127fca863c618526e2d9f3631d96a
SCAREcrow0367 <128756851+SCAREcrow0367@users.noreply.github.com>
2023-05-04 21:47:59 -0700

    Add files via upload
    Add collection

---
+++ ClassesandObject Video/ClassesandObject Video/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesandObject_Video
+{
+    class Person
+    {
+        // Member Variable Declaration
+        // Instance Variable Declaration
+
+        public string Name;
+        public int Age;
+
+    }
+}
---
+++ ClassesandObject Video/ClassesandObject Video/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassesandObject_Video
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person somePerson = new Person();
+            somePerson.Name = "Kate Murphy";
+            somePerson.Age = 21;
+
+            Person anotherPerson = new Person();
+            anotherPerson.Name = "Kyler Moore";
+            anotherPerson.Age = 25;
+
+            Person oldestPerson = somePerson;
+
+            anotherPerson = null;
+            somePerson = null;
+            oldestPerson = null; 
+        }
+    }
+}
